75bfa3b24bd296c0664e0f74cac1d38fe3b5987d
neemakhati <neemakhati11@gmail.com>
2023-07-15 00:54:08 +0545

    RESTORED WORKING

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { LoginScreen, SignupScreen, ForgotPasswordScreen, SettingsScreen } from '../screens';
+import { LoginScreen, SignupScreen, ForgotPasswordScreen } from '../screens';
 
 const Stack = createStackNavigator();
 
